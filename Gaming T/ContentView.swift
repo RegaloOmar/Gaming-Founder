419154e506ff45072fb947904b1f6640b13e3cf8
Omar Regalado <regaladomendozaomar@gmail.com>
2023-04-19 10:10:31 -0600

    Initial Commit

---
+++ Gaming T/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Gaming T
+//
+//  Created by Omar Regalado Mendoza on 19/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gaming T/Gaming_TApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gaming_TApp.swift
+//  Gaming T
+//
+//  Created by Omar Regalado Mendoza on 19/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Gaming_TApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
